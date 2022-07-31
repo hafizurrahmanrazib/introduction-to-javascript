f09bd8b34a84334b50b6e443b5ceb611595df5ed
hafizurrahmanrazib <hafizur201611044@gmail.com>
2022-07-31 19:36:49 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,19 @@
+// vary + able = variable
+
+// numeric type
+var price = 13;
+var age = 17;
+var temperature = 38;
+var weight = 65;
+var money = 100;
+var height = 6;
+
+// string type
+var person = "sodor uddin";
+var location = "andor killa bandorbon";
+var special = 'alia bhatt';
+
+// boolean type 
+var serious = true;
+var isFullMarks = true;
+var isSingle = false;
